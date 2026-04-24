edb2bacaade343e7700f30d30eb87cdbfe2c1ff7
M-Talha183 <ullahehsan016@gmail.com>
2026-04-24 22:45:46 +0500

    Add new api project

--- api_smiu_adbms/index.js
+++ api_smiu_adbms/index.js
@@ -2,3 +2,19 @@ const express = require('express');
 const cors = require('cors');
 const { Pool } = require('pg');
 require('dot-env').config();
+
+let app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+}   
+)
+
+app.use(cors());
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});

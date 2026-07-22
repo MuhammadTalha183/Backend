baf4be099914c2036fb811680147d7981d2ffb23
M-Talha183 <ullahehsan016@gmail.com>
2026-07-22 22:51:29 +0500

    connect database and fetch data

--- backend-revesion/index.js
+++ backend-revesion/index.js
@@ -1,10 +1,21 @@
 import express from "express";
-
+import pool  from "./config/db.js";
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
+
+app.get("/students",  async (req , res)=>{
+  try {
+    const result = await pool.query("select * from students");
+    res.json(result.rows)
+  } catch (error) {
+    console.error("Error fetching students", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+
+})
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");

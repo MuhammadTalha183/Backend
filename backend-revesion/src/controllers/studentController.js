86e2000ed168e7366c31527bf709cb2caa04b88b
M-Talha183 <ullahehsan016@gmail.com>
2026-07-25 20:57:26 +0500

    Add dynmaic id login in backend revesion

--- backend-revesion/src/controllers/studentController.js
+++ backend-revesion/src/controllers/studentController.js
@@ -10,3 +10,19 @@ export const getAllStudents = async (req , res)=>{
   }
 
 }
+
+// dynamic student 
+
+export const getStudentByID = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const result = await pool.query("SELECT * FROM students WHERE id = $1", [id]);
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: "Student not found" });
+    }
+    res.json(result.rows[0]);
+  } catch (error) {
+    console.error("Error fetching student", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};

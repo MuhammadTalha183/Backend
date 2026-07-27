4cab833f00e822a9adff4ce424644407253a26bc
M-Talha183 <ullahehsan016@gmail.com>
2026-07-27 21:54:48 +0500

    Add delete route

--- backend-revesion/index.js
+++ backend-revesion/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import pool  from "./config/db.js";
 import studentRouter from "./src/routes/studentRoutes.js";
-import { getStudentByID } from "./src/controllers/studentController.js";
+import { getStudentByID , deleteStudent} from "./src/controllers/studentController.js";
 const app = express();
 
 // app.get("/", (req, res) => {
@@ -12,6 +12,8 @@ app.use("/students", studentRouter);
 
 //  dynamic student route use 
 app.use("/students/:id", getStudentByID);
+// Delete the specfic student route use
+app.use("/studentsDelete/:id", deleteStudent);
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
